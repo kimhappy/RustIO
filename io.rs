599f46f4304e08425ee6b6354d94f9c555ff3a34
POMMI3R <50399467+POMMI3R@users.noreply.github.com>
2020-07-26 23:23:29 +0900

    Update io.rs

--- io.rs
+++ io.rs
@@ -1,5 +1,5 @@
-use std::io::{ Read, Write, BufReader, BufWriter, BufRead };
-use std::fs::File;
+// Rust IO struct for Baekjoon Online Judge
+// https://github.com/POMMI3R/RustIO
 
 struct IO< IT: std::io::Read, OT: std::io::Write > {
     input : BufReader< IT >,
@@ -24,18 +24,30 @@ impl< IT: std::io::Read, OT: std::io::Write > IO< IT, OT > {
         }
     }
 
+    fn line(&mut self) -> String {
+        let mut s = String::new();
+        self.input.read_line(&mut s).unwrap();
+        s
+    }
+
+    fn all_line(&mut self) -> Vec< String > {
+        let mut s = String::new();
+        self.input.read_to_string(&mut s).unwrap();
+        s.split('\n').map(|s| String::from(s)).collect()
+    }
+
     fn l2v< U: std::str::FromStr >(&mut self) -> Vec< U >
     where < U as std::str::FromStr >::Err: std::fmt::Debug {
         let mut s = String::new();
         self.input.read_line(&mut s).unwrap();
-        s.trim().split_whitespace().map(|s| s.parse().unwrap()).collect()
+        s.split_whitespace().map(|s| s.parse().unwrap()).collect()
     }
 
     fn a2v< U: std::str::FromStr >(&mut self) -> Vec< U >
     where < U as std::str::FromStr >::Err: std::fmt::Debug {
         let mut s = String::new();
         self.input.read_to_string(&mut s).unwrap();
-        s.trim().split_whitespace().map(|s| s.parse().unwrap()).collect()
+        s.split_whitespace().map(|s| s.parse().unwrap()).collect()
     }
 
     fn write1< U: std::string::ToString >(&mut self, x: U) {
@@ -51,23 +63,5 @@ impl< IT: std::io::Read, OT: std::io::Write > IO< IT, OT > {
 impl< IT: std::io::Read, OT: std::io::Write > Drop for IO< IT, OT > {
     fn drop(&mut self) {
         self.output.write_all(self.write_buffer.as_bytes()).unwrap();
-    }
-}
-
-fn main() {
-    // Examples
-    let mut cio = IO::new(std::io::stdin(), std::io::stdout());
-    let mut fio = IO::with_capacity(File::open("input.txt").unwrap(), File::create("output.txt").unwrap(), 128);
-
-    let cv = cio.l2v::< String >();
-
-    for i in cv {
-        cio.write1(i);
-    }
-
-    let fv = fio.a2v::< i32 >();
-
-    for i in fv {
-        fio.write2(i, '\n');
     }
 }
